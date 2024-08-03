1cd3419cf5eeb93b4d441e877e61c14b12c5200b
kunkun <540610423@qq.com>
2024-08-03 16:46:01 +0800

    feat: three.weapp.js & move image to oss

--- miniprogram/pages/RobotContact/RobotContact.ts
+++ miniprogram/pages/RobotContact/RobotContact.ts
@@ -1,19 +1,70 @@
 // pages/RobotContact/RobotContact.ts
+const THREE = require('../../asset/libs/three.weapp.min.js')
+const { OrbitControls } = require('../../asset/libs/OrbitControls.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    ws_url: ""
+    ws_url: "",
+    canvasId: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
+    wx.createSelectorQuery()
+      .select('#c')
+      .node()
+      .exec((res) => {
+        const canvas = THREE.global.registerCanvas(res[0].node);
+        this.setData({
+          canvasId: canvas._canvasId
+        })
+        const camera = new THREE.PerspectiveCamera(70, canvas.width / canvas.height, 1, 1000);
+        camera.position.z = 500;
+        const scene = new THREE.Scene();
+        // scene.background = new THREE.Color(0x000000,0);
+        const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+
+        const controls = new OrbitControls(camera, renderer.domElement);
+        // controls.enableDamping = true;
+        // controls.dampingFactor = 0.25;
+        // controls.enableZoom = false;
+        camera.position.set(200, 200, 500);
+        controls.update();
+        const geometry = new THREE.BoxBufferGeometry(200, 200, 200);
+
+        const texture = new THREE.TextureLoader().load('./pikachu.png');
+        const material = new THREE.MeshBasicMaterial({ map: texture });
+
+        // const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+        const mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
+
+        // renderer.setPixelRatio(wx.getSystemInfoSync().pixelRatio);
+        // renderer.setSize(canvas.width, canvas.height);
+
+        function onWindowResize() {
+          camera.aspect = window.innerWidth / window.innerHeight;
+          camera.updateProjectionMatrix();
+          renderer.setSize(canvas.width, canvas.height);
+        }
+        function render() {
+          canvas.requestAnimationFrame(render);
+          // mesh.rotation.x += 0.005;
+          // mesh.rotation.y += 0.01;
+          controls.update();
+          renderer.render(scene, camera);
+        }
+
+        render()
+      })
 
   },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -40,27 +91,19 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload() {
-
+    THREE.global.unregisterCanvas(this.data.canvasId)
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
+  touchStart(e: any) {
+    console.log('canvas', e)
+    THREE.global.touchEventHandlerFactory('canvas', 'touchstart')(e)
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
+  touchMove(e: any) {
+    // console.log('canvas', e)
+    THREE.global.touchEventHandlerFactory('canvas', 'touchmove')(e)
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+  touchEnd(e: any) {
+    console.log('canvas', e)
+    THREE.global.touchEventHandlerFactory('canvas', 'touchend')(e)
+  },
 })
